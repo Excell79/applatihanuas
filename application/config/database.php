1772eaa6b5df1094765f7fffc7698ddd6757e3e7
Excell79 <excellsmrg@gmail.com>
2023-06-15 09:30:09 +0700

    latihan UAS

--- application/config/database.php
+++ application/config/database.php
@@ -116,3 +116,11 @@ $capsule->addConnection([
     'collation' => 'utf8mb4_unicode_ci',
     'prefix' => '',
 ]);
+
+$capsule->setEventDispatcher(new Dispatcher(new Container));
+
+// Make this Capsule instance available globally via static methods... (optional)
+$capsule->setAsGlobal();
+
+// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
+$capsule->bootEloquent();
